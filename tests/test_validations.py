09059472a376bda8758af3274bcee232ac68bd27
marcsello <punkosdmarcell@rocketmail.com>
2021-04-07 17:17:28 +0200

    Added validation tests

---
+++ tests/test_validations.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+"""Tests for `pyprocsync` package."""
+
+import pytest
+import struct
+import time
+
+from pyprocsync import ProcSync
+from pyprocsync import TimeOutError, TooLateError
+
+
+@pytest.fixture
+def redis_stub(mocker):
+    redis_stub = mocker.MagicMock()
+    redis_stub.incr = mocker.MagicMock(return_value=1)
+
+    pubsub_stub = mocker.MagicMock()
+
+    def return_valid_message(*args, **kwargs):
+        cont_time = time.time() + 0.5
+        return {
+            'channel': b"continue::test",
+            'data': struct.pack("!d", cont_time)
+        }
+
+    pubsub_stub.get_message = mocker.MagicMock(side_effect=return_valid_message)
+
+    redis_stub.pubsub = mocker.MagicMock(return_value=pubsub_stub)
+
+    yield redis_stub
+
+
+@pytest.fixture
+def procsync(redis_stub):
+    yield ProcSync(redis_stub)
+
+
+def test_ctor_delay_negative(redis_stub):
+    with pytest.raises(ValueError):
+        a = ProcSync(redis_stub, delay=-1.0)
+
+
+def test_ctor_delay_notfloat(redis_stub):
+    with pytest.raises(ValueError):
+        a = ProcSync(redis_stub, delay='a')
+
+
+def test_ctor_delay_float(redis_stub):
+    a = ProcSync(redis_stub, delay=1.2)
+    assert a._delay == 1.2
+
+
+def test_sync_timeout_negative(procsync):
+    with pytest.raises(ValueError):
+        procsync.sync("test", 2, timeout=-1.0)
+
+
+def test_sync_timeout_notfloat(procsync):
+    with pytest.raises(ValueError):
+        procsync.sync("test", 2, timeout='a')
+
+
+def test_sync_timeout_float(procsync):
+    procsync.sync("test", 2, timeout=1.0)
+
+
+def test_sync_timeout_none(procsync):
+    procsync.sync("test", 2, timeout=None)
+
+
+def test_sync_nodes_zero(procsync):
+    with pytest.raises(ValueError):
+        procsync.sync("test", 0)
+
+
+def test_sync_nodes_float(procsync):
+    with pytest.raises(ValueError):
+        procsync.sync("test", 1.0)
+
+
+def test_sync_nodes_notfloat(procsync):
+    with pytest.raises(ValueError):
+        procsync.sync("test", 'a')
+
+
+def test_sync_event_name_str(procsync):
+    procsync.sync("test", 2)
+
+
+def test_sync_event_name_bytes(procsync):
+    with pytest.raises(ValueError):
+        procsync.sync(b"test", 2)
+
+
+def test_sync_event_name_notstr(procsync):
+    with pytest.raises(ValueError):
+        procsync.sync(None, 2)
